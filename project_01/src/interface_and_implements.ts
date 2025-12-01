3b859145e09ec8bb41480a8523c95460b9d3a34d
rooreckless <rooreckless@gmail.com>
2025-12-01 22:18:11 +0900

    抽象クラスと抽象メソッド abstract

--- project_01/src/interface_and_implements.ts
+++ project_01/src/interface_and_implements.ts
@@ -51,3 +51,58 @@ user_with_implements.save();
 
 // 2. 複数のクラスで共通の機能を持たせたいが、継承関係にはしたくない場合
 // 3. 大規模開発で、設計段階でクラスの設計図を明確にしておきたい場合
+
+
+//-------------------------------------
+// -- 抽象クラス abstract--
+console.log("---抽象クラス--abstract----------------");
+
+// 抽象クラスは、他のクラスが継承するための基底クラスとして設計されるクラス
+// 抽象クラス自体はインスタンス化できない。
+// 抽象クラスはabstractキーワードを使って定義される。
+
+// 抽象クラスは、共通のプロパティやメソッドを持ち、
+// さらに抽象メソッド(abstract method)を定義することができる。
+// 抽象メソッドは、派生クラスで必ず実装しなければならないメソッドであり、
+// 抽象クラス内では実装されない。
+abstract class EmailSender{
+    // コンストラクタ(ワンライナー形式)
+    constructor(protected from: string){
+
+    }
+    // 抽象メソッドの定義
+    abstract send(to: string, subject: string, body: string): boolean;
+    // 抽象クラスで、通常のメソッドの定義は可能
+    validateEmail(email: string): boolean{
+        return email.includes("@");
+    }
+}
+
+// 抽象クラスEmailSenderを継承したDevEmailSenderクラス
+class DevEmailSender extends EmailSender{
+    constructor(from: string){
+        super(from);
+    }
+    // sendメソッドの実装は必須(抽象クラスEmailSenderでabstractとして定義されているため)
+    send(to: string, subject: string, body: string): boolean{
+        if (!this.validateEmail(to)){
+            console.log("無効なメールアドレスです:", to);
+            return false;
+        }
+        console.log(`[DevEmailSender] メール送信シミュレーション From: ${this.from}, To: ${to}, Subject: ${subject}, Body: ${body}`);
+        return true;
+    }
+}
+
+const dev_mailer = new DevEmailSender("system@example.com");
+dev_mailer.send("test@example.com", "おしらせ", "テストメールです");
+
+
+//------------------------------------
+// 抽象クラスとinterfaceの違い
+// 抽象クラスは共通のプロパティやメソッドの実装を提供できるが、interfaceは型の設計図を提供するだけで、実装は持たない。
+// クラスは、1つの抽象クラスしか継承できないが、複数のinterfaceをimplementsできる。
+// 抽象クラスは状態(プロパティ)を持てるが、interfaceは状態を持てない。
+// 抽象クラスは継承関係を示すのに対し、interfaceは実装関係を示す。
+
+// まとめると、抽象クラスは共通の実装を提供したい場合に使い、interfaceは異なるクラス間で共通の設計図を提供したい場合に使う。
