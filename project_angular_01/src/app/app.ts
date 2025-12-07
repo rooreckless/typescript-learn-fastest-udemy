f359524c947a7b08fdea00874fa847f88b5cb214
rooreckless <rooreckless@gmail.com>
2025-12-07 22:26:44 +0900

    配列プロパティを作成して、テンプレートで@for文で回して描画してみる

--- project_angular_01/src/app/app.ts
+++ project_angular_01/src/app/app.ts
@@ -16,8 +16,23 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ``,
+  // テンプレート内で@for文を使い、users配列の要素ごとに描画する
+  // track句で各要素の一意な識別子を指定する(trackの指定は必須)
+  template: `
+  usersリスト: <br>
+  @for(user of users; track user.id){
+    <div>user.id = {{user.id}}: user.name = {{user.name}}</div>
+  }
+  `,
 })
-export class App {}
+export class App {
+  // 配列(中がオブジェクト)なプロパティusersを定義
+  users:Array<{id:number,name:string}> = [
+    {id:0,name:'山田太郎'},
+    {id:1,name:'鈴木花子'},
+    {id:2,name:'佐藤次郎'},
+    {id:3,name:'田中三郎'},
+  ];
+}
 
 
