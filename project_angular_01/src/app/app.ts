a0620ce4ad466c4644be48f03d50696d286d58b2
rooreckless <rooreckless@gmail.com>
2025-12-07 21:27:35 +0900

    テンプレートの{{}}で計算をすることもできる

--- project_angular_01/src/app/app.ts
+++ project_angular_01/src/app/app.ts
@@ -16,9 +16,9 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateで、プロパティを表示させることができる
+  // templateで、プロパティを表示させることができる({}で計算もでき、number型プロパティの計算もできる)
   template: `
-    Hello {{ city }}
+    Hello {{ city }}, {{1 + 1}}, {{value*3}}
   `,
   // styleを変えると、localhost:4200にアクセスしたときの文字色が変わる
   styles: `
@@ -30,4 +30,6 @@ import {Component} from '@angular/core';
 export class App {
   // cityプロパティをコンポーネントクラスに追加(string型に型定義)
   city: string = 'San Francisco';
+  // valueプロパティをコンポーネントクラスに追加(number型に型定義)
+  value: number = 100;
 }
