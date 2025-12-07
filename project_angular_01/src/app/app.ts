5a941b4a48e3acc1e1e7857cd9bfc571209253d2
rooreckless <rooreckless@gmail.com>
2025-12-07 22:04:50 +0900

    初期状態に戻す

--- project_angular_01/src/app/app.ts
+++ project_angular_01/src/app/app.ts
@@ -15,40 +15,12 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-user',
+  selector: 'app-root',
   template: `
-    Username: {{ username }}
-  `,
-  styles: `
-    :host {
-      color: red;
-    }
+    <span>Yes, the server is running</span>
   `,
 })
-export class User {
-  username = 'youngTech';
+export class App {
+  // add the boolean property here
 }
 
-// ---コンポーネントの1つめapp-user↑と2つめapp-root↓---
-
-@Component({
-  selector: 'app-root',
-  // app-rootセレクタのテンプレートは必ず描画される
-  // ↑のapp-userセレクタのテンプレートも描画されるようにしてみる
-  template: `
-    abc<br>
-    <section class="user-section">
-      <app-user></app-user>
-    </section>
-    
-  `,
-  // export されているUserコンポーネントをここでimportして使う
-  // そのうえでapp-userセレクタをテンプレートに記述する
-  imports: [User],
-  styles: `
-    .user-section {
-      border: 4px dashed blue;
-    }
-  `,
-})
-export class App {}
