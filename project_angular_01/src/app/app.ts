9328b14778457865f41532ee4d14a11ae441022e
rooreckless <rooreckless@gmail.com>
2025-12-07 22:12:13 +0900

    テンプレートで@if-@elseを使い、プロパティの値で切り替える

--- project_angular_01/src/app/app.ts
+++ project_angular_01/src/app/app.ts
@@ -16,11 +16,26 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // テンプレートで@if-@else構文を使用
+  // Appコンポーネントのbooleanなプロパティで切り替える
   template: `
+    @if(isServerRunning){
     <span>Yes, the server is running</span>
+    }
+    @else{
+    <span>No, the server is <span class="not">NOT</span> running</span>
+    }    
   `,
+  // if文のどっちかがわかりやすくなるようスタイルを指定
+  styles: [`
+    .not {
+      color: red;
+      font-weight: bold;
+    }
+  `]
 })
 export class App {
-  // add the boolean property here
+  // boolean型プロパティを作成(手動でtrue/falseを切り替えて挙動確認をする)
+  isServerRunning: boolean = false;
 }
 
