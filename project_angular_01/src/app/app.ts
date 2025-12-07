3ea60eeeec75b224625f5f570ff2de7c261206e6
rooreckless <rooreckless@gmail.com>
2025-12-08 00:12:19 +0900

    クリックで表示/非表示のイベントに対応

--- project_angular_01/src/app/app.ts
+++ project_angular_01/src/app/app.ts
@@ -18,18 +18,35 @@ import {Component} from '@angular/core';
   
   selector: 'app-root',
   // テンプレートのsection部分にマウスオーバーで秘密のメッセージを表示する
+  // ボタンをクリックするとメッセージを表示/非表示切り替え
   template: `  
     <section (mouseover)="showSecretMessage()">
       There's a secret message for you, hover to reveal 👀
       {{ message }}
     </section>
+    <br>
+    <button (click)="showSecretMessage2()">Show Secret Message / Delete Message</button>
+    <div>@if(isButtonClicked){{{message2}}}
+      
+    </div>
   `,
 })
 export class App {
-  message = '';
+  message:string = '';
+  message2:string = "Clicked! 🎉";
+  isButtonClicked:boolean= false;
+  
   // ただの関数だが、テンプレートのイベントにバインドさせて、イベントハンドラとする
   showSecretMessage() {
     this.message = 'Angular is awesome! 🚀';
+  }
+
+  showSecretMessage2(){
+    if(this.isButtonClicked){
+      this.isButtonClicked = false;
+    }else{
+      this.isButtonClicked = true;
+    }
   }
 }
 
