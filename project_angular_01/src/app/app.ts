9b054b485f8e15eaaedf9552c45c03ea448e3e8a
rooreckless <rooreckless@gmail.com>
2025-12-07 22:14:27 +0900

    初期状態に戻す

--- project_angular_01/src/app/app.ts
+++ project_angular_01/src/app/app.ts
@@ -16,26 +16,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // テンプレートで@if-@else構文を使用
-  // Appコンポーネントのbooleanなプロパティで切り替える
-  template: `
-    @if(isServerRunning){
-    <span>Yes, the server is running</span>
-    }
-    @else{
-    <span>No, the server is <span class="not">NOT</span> running</span>
-    }    
-  `,
-  // if文のどっちかがわかりやすくなるようスタイルを指定
-  styles: [`
-    .not {
-      color: red;
-      font-weight: bold;
-    }
-  `]
+  template: ``,
 })
-export class App {
-  // boolean型プロパティを作成(手動でtrue/falseを切り替えて挙動確認をする)
-  isServerRunning: boolean = false;
-}
+export class App {}
+
 
