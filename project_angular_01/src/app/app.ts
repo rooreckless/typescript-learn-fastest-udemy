419e6b4e851ed7e604b19e74eedbd5b26a374060
rooreckless <rooreckless@gmail.com>
2025-12-07 21:13:29 +0900

    公式の初期状態にする

--- project_angular_01/src/app/app.ts
+++ project_angular_01/src/app/app.ts
@@ -1,12 +1,28 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { Component, signal } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [RouterOutlet],
+//   templateUrl: './app.html',
+//   styleUrl: './app.css'
+// })
+// export class App {
+//   protected readonly title = signal('project_angular_01');
+// }
+
+
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  template: `
+    Hello
+  `,
+  styles: `
+    :host {
+      color: blue;
+    }
+  `,
 })
-export class App {
-  protected readonly title = signal('project_angular_01');
-}
+export class App {}
