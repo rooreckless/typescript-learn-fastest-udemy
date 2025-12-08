35d62ad3dffbeb763e29c7de9074abd7fab346e5
rooreckless <rooreckless@gmail.com>
2025-12-08 23:59:03 +0900

    初期状態に戻す(親と子コンポーネント状態)

--- project_angular_01/src/app/app.ts
+++ project_angular_01/src/app/app.ts
@@ -13,28 +13,19 @@
 
 
 import {Component} from '@angular/core';
-import {Child} from './child';
+import {Comments} from './comments';
 
 @Component({
-  // 親コンポーネントでは、addItemEventを受け取ったら、addItemメソッドを呼び出す
-  // items配列の長さの描画と、items配列内要素を繰り返し表示する
-  // = 子コンポーネントのボタンが押されると、子コンポーネントからイベントで'🐢'が送信され、
-  // 親コンポーネントのitems配列に追加されて描画に利用される
   selector: 'app-root',
   template: `
-    <app-child (addItemEvent)="addItem($event)" />
-    <p>🐢 all the way down {{ items.length }}</p>
-    <span>@for(item of items; track item){{{item}}}</span>
+    <div>
+      <h1>How I feel about Angular</h1>
+      <article></article>
+      <comments />
+    </div>
   `,
-  imports: [Child],
+  // 子コンポーネントcommentsを使うためにimportsに追加
+  imports: [Comments],
 })
-export class App {
-  items = new Array();
-
-  // addItemEventで受け取ったときに実行されるメソッド
-  // addItemメソッドは、受け取った文字列をitems配列に追加する
-  addItem(item: string) {
-    this.items.push(item);
-  }
-}
+export class App {}
 
