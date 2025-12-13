346c055b17b8ba28f0eae0f7db56613a0876e3f1
rooreckless <rooreckless@gmail.com>
2025-12-14 02:58:39 +0900

    リアクティブフォームを体験

--- project_angular_01/src/app/app.ts
+++ project_angular_01/src/app/app.ts
@@ -13,12 +13,15 @@
 
 
 import {Component} from '@angular/core';
-
-
+//リアクティブフォームモジュールたちをインポート
+import { ReactiveFormsModule, FormControl, FormGroup  } from '@angular/forms';
+//formタグの中は、label内でinputな点は一緒だが、inputで双方向バインディングしていない。
+//formControlNameディレクティブを使って、フォームコントロールとinput要素を関連付けている。
 @Component({
   selector: 'app-root',
+  // フォームタグには、formGroup属性についてはプロパティバインディングを使い、profileFormフォームグループをバインドしている。
   template: `
-    <form>
+    <form [formGroup]="profileForm">
       <label>
         Name
         <input type="text" formControlName="name" />
@@ -27,10 +30,26 @@ import {Component} from '@angular/core';
         Email
         <input type="email" formControlName="email" />
       </label>
-      <button type="submit">Submit</button>
+      <button type="submit" (click)="onSubmit()">Submit</button>
     </form>
+    <hr/>
+    <h2>Profile Form</h2>
+    <p>Name: {{ profileForm.value.name }}</p>
+    <p>Email: {{ profileForm.value.email }}</p>
   `,
-  imports: [],
+  // ↑フォームに入力さらた値は、formGroup属性にバインドされた変数.value.プロパティ名で取得できる。
+  imports: [ReactiveFormsModule],
 })
-export class App {}
+export class App {
+  //フォームグループを作成し、inputタグのformControlNameのnameとemailのフォームコントロールを追加
+  profileForm = new FormGroup({
+    // 以下はフォームタグ内のinputタグで使われているformControlNameと一致させる必要がある。
+    name: new FormControl(''),
+    email: new FormControl(''),
+  });
+
+  onSubmit() {
+    alert(this.profileForm.value.name + "--" + this.profileForm.value.email);
+  }
+}
 
