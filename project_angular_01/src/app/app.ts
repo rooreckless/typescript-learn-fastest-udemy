3152afd641ae9a94db2994326086ff05b165497e
rooreckless <rooreckless@gmail.com>
2025-12-13 22:09:59 +0900

    app.tsでもルーティングを使えるようにし、navタグを追加

--- project_angular_01/src/app/app.ts
+++ project_angular_01/src/app/app.ts
@@ -13,13 +13,24 @@
 
 
 import {Component} from '@angular/core';
+//ルーティングを有効化するためインポート
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  // router-outletをtemplateで使い、navタグで簡単なナビゲーションを追加
   template: `
     <h1>Welcome to Angular!</h1>
+    <nav>
+      <a href="/">Home</a>
+      |
+      <a href="/user">User</a>
+    </nav>
+    <router-outlet />
   `,
-  imports: [],
+  //ルーティングを有効化するため↑でインポートしたRouterOutletをimportsに追加
+  //これで、template内で<router-outlet>が使えるようになる
+  imports: [RouterOutlet],
 })
 export class App {}
 
