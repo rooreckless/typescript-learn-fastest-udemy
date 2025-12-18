a8ef0ab579d35032417fbdf76b0cc69495066f9c
rooreckless <rooreckless@gmail.com>
2025-12-18 21:55:57 +0900

    初期状態に戻す

--- project_angular_01/src/app/app.ts
+++ project_angular_01/src/app/app.ts
@@ -11,32 +11,15 @@
 //   protected readonly title = signal('project_angular_01');
 // }
 
-// コンストラクタベースでの依存性注入(サービスをnewしなくても使えるようにする)にはinjectはなくてもいい
 import {Component} from '@angular/core';
-import {CarService} from './car.service';
+
 @Component({
   selector: 'app-root',
   template: `
-  <p>display = {{display}}</p>
-    `,
+    {{ username }}
+  `,
   imports: [],
 })
 export class App {
-  // injectを使う場合(Angular14以降)
-  // carService = inject(CarService);
-  display: string;
-
-  // -----
-  // car.service.tsからCarServiceクラスをコンストラクタベースで注入(古いAngularでのやり方)
-  constructor(private carService: CarService) {
-    this.display = this.carService.getCars().join(' ⭐️ ');
-  }
-  // ただし、このやり方だと、carServiceはprivateなので、
-  // テンプレート内で直接carService.getCars()などは使えなくなる。
-  // -----
-
-
-  // injectを使う場合が以下のようになる。
-  // display = this.carService.getCars().join("☆");
+  username = 'yOunGTECh';
 }
-
