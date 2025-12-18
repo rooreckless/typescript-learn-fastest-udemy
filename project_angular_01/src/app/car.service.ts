fbfb60d606d54ebd7a7415883a20e91b9dbc1870
rooreckless <rooreckless@gmail.com>
2025-12-18 21:33:08 +0900

    初期状態は、前回の続きからそのままでいい。

--- project_angular_01/src/app/car.service.ts
+++ project_angular_01/src/app/car.service.ts
@@ -3,7 +3,7 @@
 // CarServiceクラスを、newを使わなくても使えるようにするためIngetcatbleデコレータを使う
 // (注入される側=サービス側はこれだけでいい)
 import {Injectable} from '@angular/core';
-// 抽象クラスをimplementsして型を合わせる(=依存性逆転のため、サービス側が守らなければならない実装ルールをインポート)
+// 抽象クラスをimplementsして型を合わせる(=依存性逆転のため、サービス側が守らなければならない実装ルールをインポート)　
 import {AbstractCartService} from './abstract.car.service';
 
 @Injectable({ providedIn: 'root'})
