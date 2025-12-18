2f4ec659fb2100cf025a4d4c9603cda2d90fb244
rooreckless <rooreckless@gmail.com>
2025-12-18 22:05:17 +0900

    いろんなパイプを使う前の下準備

--- project_angular_01/src/app/app.ts
+++ project_angular_01/src/app/app.ts
@@ -20,10 +20,24 @@ import { UpperCasePipe } from '@angular/common';
   selector: 'app-root',
   template: `
   username = {{ username}}<br>  
-  username | uppercase = {{ username | uppercase }}
+  username | uppercase = {{ username | uppercase }}<br>
+  <hr>
+  <ul>
+      <li>Number {{ num }}</li>
+      <li>Date  {{ birthday }}</li>
+      <li>Currency  {{ cost }}</li>
+  </ul>
+  <ul>
+      <li>Number with "decimal" {{ num }}</li>
+      <li>Date with "date" {{ birthday }}</li>
+      <li>Currency with "currency" {{ cost }}</li>
+  </ul>
   `,
   imports: [UpperCasePipe],
 })
 export class App {
   username = 'yOunGTECh';
+  num = 103.1234;
+  birthday = new Date(2023, 3, 2);
+  cost = 4560.34;
 }
