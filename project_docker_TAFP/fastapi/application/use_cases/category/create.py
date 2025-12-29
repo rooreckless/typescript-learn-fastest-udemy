2ce1f7bf7f9d3d05d093a38c753b5bbbf5e6c5be
rooreckless <rooreckless@gmail.com>
2025-12-29 20:47:48 +0900

    categoryの作成時にログインユーザー名が入るようにした。(admin権限がないと使えないのはそのまま)

--- project_docker_TAFP/fastapi/application/use_cases/category/create.py
+++ project_docker_TAFP/fastapi/application/use_cases/category/create.py
@@ -6,18 +6,19 @@ from domain import CategoryEntity
 from domain.category import Name,Description,CreatedBy,UpdatedBy
 from application.services.category_service import CategoryService
 from presentation.schemas.categories import CategoryCreateRequest
-
+# from presentation.dependencies import get_current_user
+from domain.user import UserEntity
 
 class CreateCategoryUseCase:
     """カテゴリ作成ユースケース"""
 
-    def __init__(self, category_service: CategoryService):
+    def __init__(self, category_service: CategoryService,current_user: UserEntity):
         """
         Args:
             category_service: カテゴリサービス
         """
         self.category_service = category_service
-
+        self.current_user = current_user
     async def __call__(
         self,
         request: CategoryCreateRequest
@@ -35,8 +36,8 @@ class CreateCategoryUseCase:
         category = CategoryEntity.create(
             name=Name.validate_value(request.name),
             description=Description.validate_value(request.description),
-            created_by=CreatedBy.validate_value(request.created_by),
-            updated_by=UpdatedBy.validate_value(request.created_by)
+            created_by=CreatedBy.validate_value(self.current_user.name.value),
+            updated_by=UpdatedBy.validate_value(self.current_user.name.value)
         )
 
         # カテゴリを永続化
--- project_docker_TAFP/fastapi/presentation/routers/category_routes.py
+++ project_docker_TAFP/fastapi/presentation/routers/category_routes.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List
 
 from infrastructure.database import provide_db
-from presentation.dependencies import provide_category_service, provide_item_service, require_admin
+from presentation.dependencies import provide_category_service, provide_item_service, require_admin,get_current_user
 from ..schemas.common import (
     MessageResponse
 )
@@ -34,7 +34,8 @@ router = APIRouter(
 async def create_category(
     request: CategoryCreateRequest,
     db: AsyncSession = Depends(provide_db),
-    admin_user: UserEntity = Depends(require_admin)
+    admin_user: UserEntity = Depends(require_admin),
+    current_user: UserEntity = Depends(get_current_user)
 ):
     """
     新しいカテゴリを作成します
@@ -46,7 +47,7 @@ async def create_category(
     """
     from application.use_cases.category.create import CreateCategoryUseCase
     service = provide_category_service(db)
-    use_case = CreateCategoryUseCase(service)
+    use_case = CreateCategoryUseCase(service,current_user)
     return CategoryResponse.model_validate(await use_case(request))
 
 
