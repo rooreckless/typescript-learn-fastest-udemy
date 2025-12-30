041091b36b0d94496ba3da8b1fb56a4862903e5e
rooreckless <rooreckless@gmail.com>
2025-12-30 14:29:27 +0900

    Module 2: Dynamic Text with Interpolationとsignal

--- project_docker_TAFP/angular/src/app/app.component.ts
+++ project_docker_TAFP/angular/src/app/app.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  // templateUrlをいれたことで、htmlは外部ファイルにした
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // styleUrls配列をいれたことで、cssは外部ファイルにした上で、複数導入できる
+  styleUrls: ['./app.component.css']
 })
 // クラス名はAppにする必要があり、↑のselectorも'app-root'にする必要がある
 export class App {
+  // sinalを使って状態管理を行う 参考 https://zenn.dev/rdlabo/articles/4b23117adb33aa
+  // protected readonlyをつけた変数は、コンポーネントのテンプレートから「のみ」アクセス可能にする
+  // テンプレートでは{{ count() }}のように関数呼び出しの形でアクセスする
+  protected readonly count = signal(0);
+  // signalには文字列を入れることもできるし、テンプレートで{{ title() }}のように表示するのも一緒
+  // (protected readonlyを使へばテンプレートからのみアクセス可能でより安全になるが、それは状況しだい)
+  title = signal("My Recipe Box");
+
 }
