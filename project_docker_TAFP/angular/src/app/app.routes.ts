613b8722578da270cb9f027c3e0e26081ff48a20
rooreckless <rooreckless@gmail.com>
2026-01-01 01:20:07 +0900

    Module 16: Introduction to Forms（フォーム入門）リアクティブフォーム

--- project_docker_TAFP/angular/src/app/app.routes.ts
+++ project_docker_TAFP/angular/src/app/app.routes.ts
@@ -8,12 +8,14 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { RecipeList } from './recipe-list/recipe-list';
 import { RecipeDetail } from './recipe-detail/recipe-detail';
+import { RecipeForm } from './recipe-form/recipe-form';
 
 export const routes: Routes = [
   // ここにルートを追加
   // 例: { path: 'users', component: UsersComponent }
   { path: '', component: Home },
   { path: 'recipes', component: RecipeList },
-  { path: 'recipes/:id', component: RecipeDetail },  // 動的ルート追加
+  { path: 'recipe/new', component: RecipeForm },  // 新規作成用ルート（動的ルートより前に配置）
+  { path: 'recipes/:id', component: RecipeDetail },  // 動的ルート
   { path: '**', redirectTo: '' }
 ];
